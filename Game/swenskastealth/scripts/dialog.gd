149e6971f2e32959c2be84dbea6e366a17d57fe1
Paul Zavalza <zse.paul@gmail.com>
2025-02-24 19:16:36 +0100

    using temporal solution to detect click on continue button

--- Game/swenskastealth/scripts/dialog.gd
+++ Game/swenskastealth/scripts/dialog.gd
@@ -5,11 +5,13 @@ signal box_closed #Notifies other nodes if dialog box was closed
 @onready var dialog_box_message = $Control/Box/Text
 @onready var dialog_box_button =  $Control/Box/Button
 var mouse_position: Vector2
+var click_count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	dialog_box.hide()
 	dialog_box_button.connect("gui_input", _on_button_interaction)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
@@ -24,10 +26,13 @@ func _input(event):
 func _on_mentor_character_talking():
 	dialog_box.show()
 
+#This is temporal solution because the pressed signal of the button is not being tiggered
 func _on_button_interaction(event):
-	if event is InputEventMouseMotion:
-		print("reaching button")
-	else: 
-		if event is InputEventMouseButton:
-			print("click on button")
+	if event is InputEventMouseButton:
+		click_count += 1
+		print("click on button")
+		print(click_count)
+		if click_count == 2:
+			print("signal to get new dialogue")
 			dialog_box_message.text = "algo nuevo"
+			click_count = 0
