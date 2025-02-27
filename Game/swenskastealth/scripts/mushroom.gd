4f891554dbd4a8bc237b36c3ef14e3bf3ea56e4d
Thomas Laux <32910534+LauxThomas@users.noreply.github.com>
2025-02-27 21:33:27 +0100

    audio wip

--- Game/swenskastealth/scripts/mushroom.gd
+++ Game/swenskastealth/scripts/mushroom.gd
@@ -4,6 +4,8 @@ signal collected
 
 @export var value: int = 1  # How many mushrooms this gives
 @export var mushroom_type: String = "TODO: CHANGE"  # Change this per mushroom instance
+@onready var audio_player = $AudioStreamPlayer  # Reference the AudioStreamPlayer node
+@export var mushroom_audio: String = "TODO: CHANGE"
 
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
@@ -11,4 +13,5 @@ func _ready():
 func _on_body_entered(body):
 	if body.name == "Raccoon": # Thats the Player
 		body.collect_mushroom(value, mushroom_type)  # Pass the mushroom type
+		audio_player.autoplay(mushroom_audio)
 		queue_free()  # Remove the mushroom from the scene
